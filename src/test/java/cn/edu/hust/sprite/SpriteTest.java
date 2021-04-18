91c95cf91f75978790d22cfd29cb70c02dc56121
luobuyu <1528751774@qq.com>
2021-04-18 18:48:33 +0800

    '添加何旺的'

--- src/test/java/cn/edu/hust/sprite/SpriteTest.java
+++ src/test/java/cn/edu/hust/sprite/SpriteTest.java
@@ -1,21 +1,45 @@
 package cn.edu.hust.sprite;
 
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
+import cn.edu.hust.Launcher;
+import cn.edu.hust.board.*;
+
+import cn.edu.hust.npc.ghost.GhostFactory;
+
 import java.io.IOException;
+import cn.edu.hust.level.LevelFactory;
+import cn.edu.hust.level.MapParser;
+import cn.edu.hust.level.Player;
+import cn.edu.hust.level.PlayerFactory;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
+import com.google.common.collect.Lists;
+/**
+ * Verifies the Movatation of sprites.
+ *
+ * @author He Wang 
+ */
 @SuppressWarnings("magicnumber")
 public class SpriteTest {
 
     private Sprite sprite;
     private SpriteStore store;
+    private MapParser parser;
 
-    private static final int SPRITE_SIZE = 64;
+    private Player player;
+    private BasicAbstractSquare s1, s2;
+    private PlayerFactory playerFactory;
+    private BoardFactory factory;
+    private Launcher launcher;
 
+    @BeforeEach
+    void setUpPacman() {
+        launcher = new Launcher();
+        launcher.launch();
+    }
     /**
      * The common fixture of this test class is
      * a 64 by 64 pixel white sprite.
@@ -25,83 +49,208 @@ public class SpriteTest {
      */
     @BeforeEach
     public void setUp() throws IOException {
+        PacManSprites sprites = new PacManSprites();
+        parser = new MapParser(new LevelFactory(sprites, new GhostFactory(
+            sprites)), new BoardFactory(sprites));
+
+        factory = new BoardFactory(sprites);
+        playerFactory = new PlayerFactory(sprites);
+        s1 = new BasicAbstractSquare();
+        s2 = new BasicAbstractSquare();
+        launcher = new Launcher();
+        launcher.launch();
+        launcher.getAbstractGame().start();
+
         store = new SpriteStore();
         sprite = store.loadSprite("/sprite/64x64white.png");
     }
 
     /**
-     * Verifies the width of a static sprite.
+     * Test the reaction of key-right
      */
     @Test
-    public void spriteWidth() {
-        assertThat(sprite.getWidth()).isEqualTo(SPRITE_SIZE);
+    void testEast() {
+        Board b = parser
+            .parseMap(Lists.newArrayList("#####", "#   #", "#####"))
+            .getBoard();
+        AbstractSquare s1 = b.squareAt(1, 1); // first row , first col
+        AbstractSquare s2 = b.squareAt(2, 1); // third col , first row
+        Player pacMan = playerFactory.createPacMan();
+        pacMan.occupy(s1);
+        launcher.getAbstractGame().move(pacMan, Direction.EAST);
+        assertThat(pacMan.getAbstractSquare()).isEqualTo(s2);
     }
 
     /**
-     * Verifies the height of a static sprite.
+     * Test the reaction of key-left
      */
     @Test
-    public void spriteHeight() {
-        assertThat(sprite.getHeight()).isEqualTo(SPRITE_SIZE);
+    void testWest() {
+        Board b = parser
+            .parseMap(Lists.newArrayList("#####", "#   #", "#####"))
+            .getBoard();
+        AbstractSquare s1 = b.squareAt(3, 1); // first row , first col
+        AbstractSquare s2 = b.squareAt(2, 1); // third col , first row
+        Player pacMan = playerFactory.createPacMan();
+        pacMan.occupy(s1);
+        launcher.getAbstractGame().move(pacMan, Direction.WEST);
+        assertThat(pacMan.getAbstractSquare()).isEqualTo(s2);
+    }
+    /**
+     * Test the reaction of key-up
+     */
+    @Test
+    void testNorth() {
+        Board b = parser
+            .parseMap(Lists.newArrayList("#   #", "#   #", "#   #"))
+            .getBoard();
+        AbstractSquare s1 = b.squareAt(1, 1); // first row , first col
+        AbstractSquare s2 = b.squareAt(1, 0); // third col , first row
+        Player pacMan = playerFactory.createPacMan();
+        pacMan.occupy(s1);
+        launcher.getAbstractGame().move(pacMan, Direction.NORTH);
+        assertThat(pacMan.getAbstractSquare()).isEqualTo(s2);
+    }
+    /**
+     * Test the reaction of key-up
+     */
+    @Test
+    void testSouth() {
+        Board b = parser
+            .parseMap(Lists.newArrayList("#   #", "#   #", "#   #"))
+            .getBoard();
+        AbstractSquare s1 = b.squareAt(1, 1); // first row , first col
+        AbstractSquare s2 = b.squareAt(1, 2); // third col , first row
+        Player pacMan = playerFactory.createPacMan();
+        pacMan.occupy(s1);
+        launcher.getAbstractGame().move(pacMan, Direction.SOUTH);
+        assertThat(pacMan.getAbstractSquare()).isEqualTo(s2);
+    }
+    /**
+     * Test the movatation in east boundary
+     */
+    @Test
+    void testEastBoundary() {
+        Board b = parser
+            .parseMap(Lists.newArrayList("#####", "#   #", "#####"))
+            .getBoard();
+        AbstractSquare s1 = b.squareAt(3, 1); // first row , first col
+        AbstractSquare s2 = b.squareAt(3, 1); // third col , first row
+        Player pacMan = playerFactory.createPacMan();
+        pacMan.occupy(s1);
+        launcher.getAbstractGame().move(pacMan, Direction.EAST);
+        assertThat(pacMan.getAbstractSquare()).isEqualTo(s2);
     }
 
     /**
-     * Verifies that an IOException is thrown when the resource could not be
-     * loaded.
-     *
-     * @throws java.io.IOException
-     *             since the sprite cannot be loaded.
+     * Test the movatation in west boundary
      */
     @Test
-    public void resourceMissing() throws IOException {
-        assertThatThrownBy(() -> store.loadSprite("/sprite/nonexistingresource.png"))
-            .isInstanceOf(IOException.class);
+    void testWestBoundary() {
+        Board b = parser
+            .parseMap(Lists.newArrayList("#####", "#   #", "#####"))
+            .getBoard();
+        AbstractSquare s1 = b.squareAt(1, 1); // first row , first col
+        AbstractSquare s2 = b.squareAt(1, 1); // third col , first row
+        Player pacMan = playerFactory.createPacMan();
+        pacMan.occupy(s1);
+        launcher.getAbstractGame().move(pacMan, Direction.WEST);
+        assertThat(pacMan.getAbstractSquare()).isEqualTo(s2);
     }
 
     /**
-     * Verifies that an animated sprite is correctly cut from its base image.
+     * Test the movatation in north boundary
      */
     @Test
-    public void animationWidth() {
-        AnimatedSprite animation = store.createAnimatedSprite(sprite, 4, 0,
-            false);
-        assertThat(animation.getWidth()).isEqualTo(16);
+    void testNorthBoundary() {
+        Board b = parser
+            .parseMap(Lists.newArrayList("#####", "#   #", "#####"))
+            .getBoard();
+        AbstractSquare s1 = b.squareAt(1, 1); // first row , first col
+        AbstractSquare s2 = b.squareAt(1, 1); // third col , first row
+        Player pacMan = playerFactory.createPacMan();
+        pacMan.occupy(s1);
+        launcher.getAbstractGame().move(pacMan, Direction.NORTH);
+        assertThat(pacMan.getAbstractSquare()).isEqualTo(s2);
     }
 
     /**
-     * Verifies that an animated sprite is correctly cut from its base image.
+     * Test the movatation in south boundary
      */
     @Test
-    public void animationHeight() {
-        AnimatedSprite animation = store.createAnimatedSprite(sprite, 4, 0,
-            false);
-        assertThat(animation.getHeight()).isEqualTo(64);
+    void testSouthBoundary() {
+        Board b = parser
+            .parseMap(Lists.newArrayList("#####", "#   #", "#####"))
+            .getBoard();
+        AbstractSquare s1 = b.squareAt(1, 1); // first row , first col
+        AbstractSquare s2 = b.squareAt(1, 1); // third col , first row
+        Player pacMan = playerFactory.createPacMan();
+        pacMan.occupy(s1);
+        launcher.getAbstractGame().move(pacMan, Direction.SOUTH);
+        assertThat(pacMan.getAbstractSquare()).isEqualTo(s2);
     }
 
     /**
-     * Verifies that an split sprite is correctly cut from its base image.
+     * Test the move east to barrier
      */
     @Test
-    public void splitWidth() {
-        Sprite split = sprite.split(10, 11, 12, 13);
-        assertThat(split.getWidth()).isEqualTo(12);
+    void testEastMove() {
+        Board b = parser
+            .parseMap(Lists.newArrayList("#####", "#   #", "#####"))
+            .getBoard();
+        AbstractSquare s1 = b.squareAt(3, 1); // first row , first col
+        AbstractSquare s2 = b.squareAt(3, 1); // third col , first row
+        Player pacMan = playerFactory.createPacMan();
+        pacMan.occupy(s1);
+        launcher.getAbstractGame().move(pacMan, Direction.EAST);
+        assertThat(pacMan.getAbstractSquare()).isEqualTo(s2);
     }
 
     /**
-     * Verifies that an split sprite is correctly cut from its base image.
+     * Test the move west to barrier
      */
     @Test
-    public void splitHeight() {
-        Sprite split = sprite.split(10, 11, 12, 13);
-        assertThat(split.getHeight()).isEqualTo(13);
+    void testWestMove() {
+        Board b = parser
+            .parseMap(Lists.newArrayList("#####", "#   #", "#####"))
+            .getBoard();
+        AbstractSquare s1 = b.squareAt(1, 1); // first row , first col
+        AbstractSquare s2 = b.squareAt(1, 1); // third col , first row
+        Player pacMan = playerFactory.createPacMan();
+        pacMan.occupy(s1);
+        launcher.getAbstractGame().move(pacMan, Direction.WEST);
+        assertThat(pacMan.getAbstractSquare()).isEqualTo(s2);
+    }
+    /**
+     * Test the movatation in north boundary
+     */
+    @Test
+    void testNorthMove() {
+        Board b = parser
+            .parseMap(Lists.newArrayList("#####", "#   #", "#####"))
+            .getBoard();
+        AbstractSquare s1 = b.squareAt(1, 1); // first row , first col
+        AbstractSquare s2 = b.squareAt(1, 1); // third col , first row
+        Player pacMan = playerFactory.createPacMan();
+        pacMan.occupy(s1);
+        launcher.getAbstractGame().move(pacMan, Direction.NORTH);
+        assertThat(pacMan.getAbstractSquare()).isEqualTo(s2);
     }
 
     /**
-     * Verifies that a split that isn't within the actual sprite returns an empty sprite.
+     * Test the move south to barrier
      */
     @Test
-    public void splitOutOfBounds() {
-        Sprite split = sprite.split(10, 10, 64, 10);
-        assertThat(split).isInstanceOf(EmptySprite.class);
+    void testSouthMove() {
+        Board b = parser
+            .parseMap(Lists.newArrayList("#####", "#   #", "#####"))
+            .getBoard();
+        AbstractSquare s1 = b.squareAt(1, 1); // first row , first col
+        AbstractSquare s2 = b.squareAt(1, 1); // third col , first row
+        Player pacMan = playerFactory.createPacMan();
+        pacMan.occupy(s1);
+        launcher.getAbstractGame().move(pacMan, Direction.SOUTH);
+        assertThat(pacMan.getAbstractSquare()).isEqualTo(s2);
     }
+
 }
